dae8f38ff00177ef4c78ddd5417ffe5afb1543b7
Anju Arianto <anjuarianto@gmail.com>
2024-03-16 04:03:34 +0700

    fix: layout absen karyawan on karyawan role

--- resources/views/laporan/index.blade.php
+++ resources/views/laporan/index.blade.php
@@ -23,7 +23,6 @@
             <div class="d-flex justify-content-between">
                 <div class="d-flex">
                     <div>
-                        <!-- Download SVG icon from http://tabler-icons.io/i/check -->
                         <svg xmlns="http://www.w3.org/2000/svg" class="icon alert-icon" width="24" height="24"
                              viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none"
                              stroke-linecap="round" stroke-linejoin="round">
@@ -33,7 +32,7 @@
                     </div>
                     <div>
                         <h4 class="alert-title">Success!</h4>
-                        <div class="text-secondary">{{$message}}</div>
+                        <div class="text-secondary">{{ $message }}</div>
                     </div>
                 </div>
                 <a class="btn-close" data-bs-dismiss="alert" aria-label="close"></a>
@@ -42,7 +41,7 @@
     @endif
     <div class="card">
         <div class="card-header justify-content-between align-items-center">
-            <h3 class="m-0">Tabel Laporan - {{General::setBulanToString($filter_bulan)}}, {{$filter_bulan}}
+            <h3 class="m-0">Tabel Laporan - {{ General::setBulanToString($filter_bulan) }}, {{$filter_bulan}}
                 /{{$filter_tahun}}</h3>
             <div>
                 <form action="{{route('laporan.index')}}" method="get">
